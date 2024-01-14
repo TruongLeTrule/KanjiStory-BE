2fd2dcf74762f85d6aee77882b2f175d72b4246b
Truong Le <21522732@gm.uit.edu.vn>
2024-01-14 20:55:43 +0700

    Add translate in long story

--- models/multipleKanji.js
+++ models/multipleKanji.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const MultipleKanjiStorySchema = new mongoose.Schema({
   kanji: Array,
   story: String,
+  translate: String,
   storyType: String,
   date: Date,
 });
